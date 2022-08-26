1284c71770a7f42ac3d7fe583f7a00bbb8fb5a8d
isaactobiloba <isaactobiloba7@gmail.com>
2022-08-26 14:30:07 +0100

    Add project files.

---
+++ Catalog/Repositories/InMemItemsRepository.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository
+    {
+
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Joe Abriba", Price = 30, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Sandy Abriba", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Melissa Abriba", Price = 10, CreatedDate = DateTimeOffset.UtcNow }
+
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}
