025a59c45a4cfdb653e774b3c67b584458032222
isaactobiloba <isaactobiloba7@gmail.com>
2022-08-26 19:22:07 +0100

    Create InMemItemsRepository Interface

---
+++ Catalog/Repositories/IInMemItemsRepository.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public interface IInMemItemsRepository
+    {
+        Item GetItem(Guid id);
+        IEnumerable<Item> GetItems();
+    }
+}
